0bd6a1327799ba29876c97a4beb9a4221e8fe756
Jatin Koria <jkoria@rhythmone.com>
2021-10-18 11:57:28 +0530

    first commit

---
+++ template.py
@@ -0,0 +1,15 @@
+import os
+
+dirs = ["data_given", "src", os.path.join("data", "raw"), os.path.join("data", "processed")]
+
+for dir_ in dirs:
+    os.makedirs(dir_, exist_ok=True)
+    gitkeep_path = os.path.join(dir_, ".gitkeep")
+    with open(gitkeep_path, "w") as f:
+        pass
+
+files = ["config.yaml", "dvc.yaml", "params.yaml", ".gitignore", "README.md", "requirements.txt"]
+
+for file_ in files:
+    with open(file_, "w") as f:
+        pass
