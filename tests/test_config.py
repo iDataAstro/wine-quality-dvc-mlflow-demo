b14b868bf4638b91ea7a62c9c86c9c4f100e7c88
Jatin Koria <jkoria@rhythmone.com>
2021-10-20 11:50:10 +0530

    Exception test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,17 @@
+import pytest
+
+
+class NotInRange(Exception):
+    def __init__(self, message="Value not in range"):
+        self.message = message
+        super().__init__(self.message)
+
+
+def test_NotInRange():
+    a = 5
+    with pytest.raises(NotInRange):
+        if a > 4:
+            raise NotInRange
 
 
 def test_generic():
